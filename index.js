88b27e430adea8655ef274aab18d580c72e813a5
JP Dillingham <jpdillingham@gmail.com>
2017-03-27 23:11:14 -0500

    Stubbed out basic calculator application

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
-var express = require('express')
-var app = express()
-var count = 0.0
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send("Current count: " + count)
-})
+// create a dictionary to store our counters
+var counts = { current: 0.0, previous: 0.0 }
 
-app.get('/up', function(req,res) {
-  count++
-  res.send('Counted up to: ' + count)
-})
-
-app.get('/down', function(req,res) {
-  count--
-  res.send("Counted down to: " + count)
-})
-
-app.get('/double', function(req, res) {
-  count = count * 2
-  res.send("Doubled 111 count to: " + count)
-})
-
-app.get('/halve', function(req, res) {
-  count = count / 2
-  res.send("Halved count to: " + count)
-})
-
-app.get('/set', function(req, res) {
-  if (!isNaN(req.query.count)) {
-    count = req.query.count
-    res.send("Set count to: " + count)
+// update the current count with a new value after
+// updating the previous count
+function updateCount(count) {
+  if (isNaN(count)) {
+    throw new Error("Invalid number.");
   }
-  res.status(400)
-  res.send("Must be a number.")
-})
+  counts['previous'] = counts['current'];
+  counts['current'] = count;
+}
 
+// return the JSON string of the counter dictionary
+function currentValue() {
+  return JSON.stringify(counts);
+}
+
+// redirect basic content routes to the bootstrap/jquery folder(s)
+app.use('/js', express.static('node_modules/bootstrap/dist/js'));
+app.use('/js', express.static('node_modules/jquery/dist'));
+app.use('/css', express.static('node_modules/bootstrap/dist/css'));
+app.use('/fonts', express.static('node_modules/bootstrap/fonts'));
+
+// serve static files from the 'public' folder
+app.use('/', express.static('public'));
+
+// set up api routes
+app.get('/api/count', function (request, response) {
+  response.send(JSON.stringify(counts));
+});
+
+app.get('/api/add', function(request,response) {
+  updateCount(counts['current'] + 1);
+  response.send(currentValue());
+});
+
+app.get('/api/subtract', function(request,response) {
+  updateCount(counts['current'] - 1);
+  response.send(currentValue());
+});
+
+app.get('/api/double', function(request, response) {
+  updateCount(counts['current'] * 2);
+  response.send(currentValue());
+});
+
+app.get('/api/halve', function(request, response) {
+  updateCount(counts['current'] / 2);
+  response.send(currentValue());
+});
+
+app.get('/api/set', function(request, response) {
+  updateCount(request.query.count);
+  response.send(currentValue());
+});
+
+// start the server on port 3000
+// open a browser and navigate to http://localhost:3000
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
